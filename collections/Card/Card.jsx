3da8884c64c5d088f5ec1d14a7172d166ea13903
banagreg <bogdanovagergana97@gmail.com>
2024-04-24 11:35:56 +0300

    Added Card component with its styles

--- collections/Card/Card.jsx
+++ collections/Card/Card.jsx
@@ -1,1 +1,19 @@
-// The Card to be exported goes here
+
+import Image from 'next/image'
+
+import { CardContainer, CardInfoContainer, CardInnerHeading, CardInnerText, ImageContainer } from "./elements"
+
+export const Card = ({ card }) => {
+	const { image, title, description } = card;
+	return (
+		<CardContainer >
+			<ImageContainer>
+				<Image src={image.src} alt={title} width={image.width} height={image.height} />
+			</ImageContainer>
+			<CardInfoContainer>
+				<CardInnerHeading>{card.title}</CardInnerHeading>
+				<CardInnerText>{card.description}</CardInnerText>
+			</CardInfoContainer>
+		</CardContainer>
+	);
+};
